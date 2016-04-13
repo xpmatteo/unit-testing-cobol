c17b7942929fdd36fbd2ac708c48472a13384e1a
Matteo Vaccari <mvaccari@thoughtworks.com>
2016-04-13 17:52:42 +0200

    Test-driven upcasing the first letter of the name

--- hello_test.cbl
+++ hello_test.cbl
@@ -5,44 +5,48 @@ DATA DIVISION.
    WORKING-STORAGE SECTION.
    01 userName PIC A(15).
    01 returnedMessage PIC A(30).
-   01 expectedResult PIC A(30).
+   01 expectedMessage PIC A(30).
    01 errorsCount PIC 999 VALUE 0.
 
 
 
 PROCEDURE DIVISION.
 
-  Begin.
 *> if a name is passed
     MOVE 'Jonathan' TO userName.
-    MOVE 'Hello, Jonathan!' TO expectedResult.
+    MOVE 'Hello, Jonathan!' TO expectedMessage.
     PERFORM 'CHECK-HELLO-MESSAGE'
 
 *> if no name is passed
     MOVE SPACES TO userName.
-    MOVE 'Hello, world!' TO expectedResult.
+    MOVE 'Hello, world!' TO expectedMessage.
+    PERFORM 'CHECK-HELLO-MESSAGE'
+
+*> Upcase the first letter of the name
+    MOVE 'foo' TO userName
+    MOVE 'Hello, Foo!' to expectedMessage
     PERFORM 'CHECK-HELLO-MESSAGE'
 
 *> noise in the return string is cleaned
     MOVE SPACES TO returnedMessage.
     INSPECT returnedMessage REPLACING ALL " " BY "x".
     MOVE SPACES TO userName.
-    MOVE 'Hello, world!' TO expectedResult.
+    MOVE 'Hello, world!' TO expectedMessage.
     PERFORM 'CHECK-HELLO-MESSAGE'
 
     IF errorsCount > 0 THEN
       STOP RUN RETURNING 1
     END-IF.
-    GOBACK.
+  GOBACK.
 
   CHECK-HELLO-MESSAGE.
     CALL 'HELLO-MESSAGE' USING returnedMessage, userName.
-    IF expectedResult = returnedMessage THEN
+    IF expectedMessage = returnedMessage THEN
       DISPLAY "." WITH NO ADVANCING
     ELSE
       ADD 1 TO errorsCount
       DISPLAY SPACES
-      DISPLAY "Mismatch: >" expectedResult "<"
+      DISPLAY "Mismatch: >" expectedMessage "<"
       DISPLAY "  actual: >" returnedMessage "<"
     END-IF.
 
