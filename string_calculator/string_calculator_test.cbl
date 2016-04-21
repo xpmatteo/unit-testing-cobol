8b0baa11ef13f7124d0586ca5a25762ff3065d4b
Matteo Vaccari <mvaccari@thoughtworks.com>
2016-04-21 12:55:29 +0200

    made the algorithm much more simple.... as long as we dont check that the delimiters are implemented correctly :)

--- string_calculator/string_calculator_test.cbl
+++ string_calculator/string_calculator_test.cbl
@@ -6,18 +6,14 @@ DATA DIVISION.
 *> constants
     01 newline         PIC X   VALUE x'0a'.
 
-
-*> add parameters
-    01 inputString.
-      02 prefix       PIC X(2).
-      02 newDelimiter PIC X(1).
-      02 FILLER       PIC X(99).
+*> add routine parameters
+    01 inputString    PIC X(100).
     01 result         PIC 9(10).
 
-*> add working storage
-    01 stringPointer  PIC 9(10).
+*> add routine working storage
     01 addend         PIC 9(10).
-    01 theDelimiter   PIC X(1).
+    01 stringPointer           PIC 9(10).
+    01 digit          PIC 9.
 
 *> test working storage
     01 expectedResult PIC 9(10).
@@ -29,8 +25,8 @@ TestSuite.
   MOVE 0 TO expectedResult.
   PERFORM "testAdd".
 
-  MOVE "1" TO inputString.
-  MOVE 1 TO expectedResult.
+  MOVE "123" TO inputString.
+  MOVE 123 TO expectedResult.
   PERFORM "testAdd".
 
   MOVE "1,3" TO inputString.
@@ -51,25 +47,27 @@ TestSuite.
   MOVE 3 TO expectedResult.
   PERFORM "testAdd".
 
+  MOVE SPACES TO inputString.
+  STRING "//[ab]" newline "7ab1ab2" INTO inputString.
+  MOVE 10 TO expectedResult.
+  PERFORM "testAdd".
+
 
   DISPLAY SPACE.
   GOBACK.
 
 doAdd.
-  MOVE 0 TO result.
-  IF prefix = "//" THEN
-    MOVE 5 TO stringPointer
-    MOVE newDelimiter TO theDelimiter
-  ELSE
-    MOVE 1 TO stringPointer
-    MOVE "," TO theDelimiter
-  END-IF.
-  PERFORM UNTIL stringPointer > LENGTH OF inputString
-    UNSTRING inputString
-      DELIMITED BY theDelimiter OR newline
-      INTO addend
-      WITH POINTER stringPointer
-    ADD addend TO result
+  MOVE 0 TO result
+  MOVE 0 to addend
+  PERFORM VARYING stringPointer FROM 1 BY 1 UNTIL stringPointer > LENGTH OF inputString
+    IF inputString(stringPointer:1) IS NUMERIC
+      MOVE inputString(stringPointer:1) TO digit
+      MULTIPLY addend BY 10 GIVING addend
+      ADD digit TO addend
+    ELSE
+      ADD addend TO result
+      MOVE 0 to addend
+    END-IF
   END-PERFORM.
 
 testAdd.
