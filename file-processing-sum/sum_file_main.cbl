a55f1a30c722a4e1269db124ab07723d6c43da92
Matteo Vaccari <mvaccari@thoughtworks.com>
2016-05-02 19:52:28 +0200

    simplified; didnt need to have the line length

--- file-processing-sum/sum_file_main.cbl
+++ file-processing-sum/sum_file_main.cbl
@@ -12,19 +12,16 @@ ENVIRONMENT DIVISION.
 
 DATA DIVISION.
   FILE SECTION.
-
-    FD InputFile
-    RECORD IS VARYING IN SIZE DEPENDING ON inputLineLength.
+    FD InputFile RECORD IS VARYING IN SIZE.
     01 inputFileRecord PIC X(200).
 
     FD SumFile.
-    01 SumFileRecord.
+    01 sumFileRecord.
       02 currentNumber PIC 9999.
       02 separator     PIC X.
       02 runningSum    PIC 9999.
 
   WORKING-STORAGE SECTION.
-    01 inputLineLength PIC 999.
     01 inputFileName PIC X(100).
     01 outputFileName PIC X(100).
     01 inputFileStatus PIC 9 VALUE 0.
@@ -46,9 +43,9 @@ Main.
     READ InputFile
       AT END SET endOfInputFile TO TRUE
       NOT AT END
-        MOVE inputFileRecord(1:inputLineLength) TO currentNumber
+        MOVE inputFileRecord TO currentNumber
         ADD currentNumber TO runningSum
-        WRITE SumFileRecord BEFORE ADVANCING 1 LINE
+        WRITE sumFileRecord BEFORE ADVANCING 1 LINE
     END-READ
   END-PERFORM.
 
