d69bc1572046f998a3ef2e76c5fa5374821cae11
Matteo Vaccari <vaccari@pobox.com>
2016-08-18 12:01:54 +0200

    using a proper output record

--- wc/wc.cbl
+++ wc/wc.cbl
@@ -1,9 +1,23 @@
 identification division.
 program-id. wc.
 
+data division.
+working-storage section.
+  01 outputRecord.
+    02 outputByteCount      pic ZZZZZZZ9.
+    02 outputWordCount      pic ZZZZZZZ9.
+    02 outputLineCount      pic ZZZZZZZ9.
+    02 filler               pic X value space.
+    02 inputFileName        pic X(80).
+  01 input-file-name pic X(80).
+
 procedure division.
 
-display "       0       0       0 /tmp/input.txt"
+main.
+  accept inputFileName from argument-value
+  move 0 to outputByteCount
+  move 0 to outputWordCount
+  move 0 to outputLineCount
+  display function trim(outputRecord, trailing)
 
-goback.
-
+  goback.
