46f3e5c2a2f82e19baf910e6f337c76612d01137
Matteo Vaccari <vaccari@pobox.com>
2016-08-18 17:32:55 +0200

    multiple files: failing test

--- wc/script/test.sh
+++ wc/script/test.sh
@@ -45,6 +45,21 @@ expect_wc "one line, two words with many spaces" \
   " ciao   beppe \n"\
   "       1       2      15 $input\n"
 
+expect_wc "three lines" \
+  "one \n two\n  three \n"\
+  "       3       3      19 $input\n"
+
+# many files
+echo "foo" > /tmp/input0.txt
+echo "bar baz" > /tmp/input1.txt
+cat > $expected <<EOF
+       1       1      4 /tmp/input0.txt
+       1       2      8 /tmp/input1.txt
+       2       3     12 total
+EOF
+./wc /tmp/input0.txt /tmp/input1.txt > $output
+expect_file_equal $expected $output "$message"
+
 
 report_any_failures
 
