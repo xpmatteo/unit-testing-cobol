6b66f97a62f0347d2aca2735274fcc97b50fdc82
Matteo Vaccari <vaccari@pobox.com>
2016-08-28 17:08:17 +0200

    improved error message

--- cgi/htmlgen_test.cbl
+++ cgi/htmlgen_test.cbl
@@ -5,14 +5,18 @@ data division.
 
   working-storage section.
     01 htmlOutput pic x(1000).
+    01 expected   pic x(1000).
 
 procedure division.
 
   call "htmlgen-tostring" using htmlOutput
-  if htmlOutput = "<p></p>"
+  move "<p></p>" to expected
+  if htmlOutput = expected
     display "." with no advancing
   else
     display "E"
+    display "Expected --" function trim(expected) "--"
+    display "But was  --" function trim(htmlOutput) "--"
   end-if
 
   goback.
